b7065a8220fe95cab75d4f72d83e173b1dd6e45d
SEBASTIANULIBARRI <145642465+SEBASTIANULIBARRI@users.noreply.github.com>
2024-05-11 13:23:12 -0300

    Correccion css y JS

--- CodigioTPE 3/JS/Index.js
+++ CodigioTPE 3/JS/Index.js
@@ -1,6 +1,15 @@
 //permite hacer responsivo el menu agregando o quitando la clase oculto
-document.querySelector(".barras").addEventListener("click", ()=>{document.querySelector("nav").classList.toggle("oculto");});
+document.querySelector(".barras").addEventListener("click", () => { document.querySelector("nav").classList.toggle("oculto"); });
 //muestra las politicas de privacidad agregando o quitando la clase oculto
-document.querySelector("#Politica-de-privacidad").addEventListener("click", ()=>{document.querySelector("#Politica-de-privacidad ~ p").classList.toggle("oculto");});
+document.querySelector("#Politica-de-privacidad").addEventListener("click", () => { document.querySelector("#Politica-de-privacidad ~ article").classList.toggle("oculto"); });
 //muestra los terminos de uso agregando o quitando la clase oculto
-document.querySelector("#Terminos-de-uso").addEventListener("click", ()=>{document.querySelector("#Terminos-de-uso ~ p").classList.toggle("oculto");});
+document.querySelector("#Terminos-de-uso").addEventListener("click", () => { document.querySelector("#Terminos-de-uso ~ article").classList.toggle("oculto"); });
+//Permite detallar mas la tabla cuando estas en una pantalla de tablet o mas grande
+let botonDetalles = document.querySelector("#detalle-tabla");
+botonDetalles.addEventListener("click",
+    () => {
+        document.querySelectorAll(".col-flexibles").forEach(element => {
+            element.classList.toggle("oculto");
+            botonDetalles.innerHTML = (element.classList.contains("oculto")) ? "Mostrar Detalles" : "Ocultar Detalles"
+        })
+    });
