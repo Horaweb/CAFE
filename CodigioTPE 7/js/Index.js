eafd3440a433b6f1ee5c95fe8bed800bc4503868
Horaweb <84884256+Horaweb@users.noreply.github.com>
2024-05-17 22:18:47 -0300

    Add files via upload

---
+++ CodigioTPE 7/js/Index.js
@@ -0,0 +1,52 @@
+"use strict"
+
+let body = document.querySelector("body");
+
+//permite hacer responsivo el menu agregando o quitando la clase oculto
+document.querySelector(".barras").addEventListener("click", () => {
+    document.querySelector("nav").classList.toggle("oculto");
+});
+  
+
+if(document.location.pathname.split("/").pop() ==="variedades.html"){
+  //Permite detallar mas la tabla cuando estas en una pantalla de tablet o mas grande
+  let botonDetalles = document.querySelector("#detalle-tabla");
+  botonDetalles.addEventListener("click", () => {
+    document.querySelectorAll(".col-flexibles").forEach((element) => {
+      element.classList.toggle("oculto");
+      botonDetalles.innerHTML = element.classList.contains("oculto")
+        ? "Mostrar Detalles"
+        : "Ocultar Detalles";
+    });
+  });
+}
+
+
+/*
+  CAPTCHA
+  */
+
+  
+  let form = document.querySelector("#form");
+  let captcha_ingresado = document.querySelector("#captcha_ingresado");
+  let mensaje_captcha = document.querySelector("#mensaje_captcha")
+
+ form.addEventListener("submit", validar)
+
+  const  valor_captcha = "1234" 
+
+  function validar(e){
+    e.preventDefault()
+    console.log(e)
+    console.log(captcha_ingresado)
+    if (captcha_ingresado.value === valor_captcha){
+        mensaje_captcha.innerHTML = "Su mensaje ha sido enviado. Pronto nos comunicaremos con usted! GRACIAS!";
+        
+    }
+    else{
+        mensaje_captcha.innerHTML = "Captcha INCORRECTO!";
+        
+    }
+  }
+  
+  
